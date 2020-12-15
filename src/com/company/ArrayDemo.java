dab1a5af8dd40ceba2d060771b40cbb8011b45d4
Alexi Krol <krol.mats@gmail.com>
2020-12-15 11:21:56 +0200

    First commit

---
+++ src/com/company/ArrayDemo.java
@@ -0,0 +1,64 @@
+package com.company;
+
+public class ArrayDemo {
+
+
+    public static int summaMassiva() {
+        final int[] myArray = {1, 5, 4, 5, 6};
+        int sum = 0;
+        for (int i = 0; i < myArray.length; i++) {
+            sum += myArray[i];
+        }
+        return sum;
+    }
+
+    public static double averageMassiva() {
+        int[] myArray = {1, 5, 4, 5, 6};
+
+        return summaMassiva() / myArray.length;
+    }
+
+    public static int maxNumberMassiva() {
+        int[] myArray = {1, 5, 4, 5, 6};
+        int max = 0;
+        for (int i = 0; i < myArray.length; i++) {
+            if (myArray[i] > max) {
+                max = myArray[i];
+            }
+        }
+        return max;
+    }
+
+    public static int[] addNumberArray() {
+
+        int[] arrayDouble = new int[]{2, 5, 6, 7, 9};
+        int n = arrayDouble.length;
+        int d = 6;
+
+        int[] newArray = new int[n + 1];
+        for (int i = 0; i < n; i++)
+            newArray[i] = arrayDouble[i];
+
+        newArray[n] = d;
+        return newArray;
+    }
+
+    public static void reverse( int [] array) {
+          array = new int[]{2, 4, 5, 7, 8, 9, 70};
+
+
+        for (int i = array.length - 1; i >= 0; i--)
+            System.out.print(array[i] + "  ");
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Сумма массива: " + ArrayDemo.summaMassiva());
+        System.out.println("Среднее значение значения  массива: " + ArrayDemo.averageMassiva());
+        System.out.println("Максимальное число в массиве: " + ArrayDemo.maxNumberMassiva());
+        System.out.println("Добавим число в массив : " + Arrays.toString(ArrayDemo.addNumberArray()));
+      
+
+
+    }
+
+}
+
+
